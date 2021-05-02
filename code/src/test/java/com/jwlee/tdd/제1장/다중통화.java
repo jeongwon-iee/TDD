b988a7520ef942e816bd733b0d3842b62439701e
jeongwon-iee <garden1117244@gmail.com>
2021-05-02 18:45:54 +0900

    fix(제1장): fix testMultiplier

--- code/src/test/java/com/jwlee/tdd/제1장/다중통화.java
+++ code/src/test/java/com/jwlee/tdd/제1장/다중통화.java
@@ -14,7 +14,9 @@ public class 다중통화 {
     @DisplayName("금액을 주식 수에 곱한 금액을 반환한다.")
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertThat(10).isEqualTo(five.amount);
+        Dollar product = five.times(2);
+        assertThat(10).isEqualTo(product.amount);
+        product = five.times(3);
+        assertThat(15).isEqualTo(product.amount);
     }
 }
